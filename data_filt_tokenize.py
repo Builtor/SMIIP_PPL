25cad223e122de4eb4018550fc61449a9743e867
BuilBuilLing <270120634@qq.com>
2017-08-17 15:58:47 +0800

    run 32个进程就好了。。。。实在没啥办法，多线程会报错，加锁效率估计也不高

--- data_filt_tokenize.py
+++ data_filt_tokenize.py
@@ -4,57 +4,39 @@ import all_tokenize as at
 import os
 
 words_set = at.get_words_set('words/words.txt')
-if not os.path.exists('res_tokenize'):
-    os.mkdir('res_tokenize')
+if not os.path.exists('res_tokenize_mul'):
+    os.mkdir('res_tokenize_mul')
 
 
-def jieba_tk():
-    jieba_res = open('res_tokenize/jieba_res_v1.txt', 'w')
+def mul_pro(text_in, text_out_num):
+    reload(at)
 
 
-def ltp_tk():
-    ltp_res = open('res_tokenize/ltp_res_v1.txt', 'w')
-
-
-def thulac_tk():
-    thulac_res = open('res_tokenize/thulac_res_v1.txt', 'w')
-
-
-def nlpir_tk():
-    nlpir_res = open('res_tokenize/nlpir_res_v1.txt', 'w')
 
 if __name__ == '__main__':
-
-    jieba_res = open('res_tokenize/jieba_res_v1.txt', 'w')
-
     at.load_ltp_model('../cws.model')
-    ltp_res = open('res_tokenize/ltp_res_v1.txt', 'w')
+    num = '16'
+    text_out_num = num
+    jieba_res = open('res_tokenize_mul/jieba_res_'+ text_out_num + '.txt', 'w')
+    ltp_res = open('res_tokenize_mul/ltp_res_'+ text_out_num + '.txt', 'w')
+    thulac_res = open('res_tokenize_mul/thulac_res_'+ text_out_num + '.txt', 'w')
+    nlpir_res = open('res_tokenize_mul/nlpir_res_'+ text_out_num + '.txt', 'w')
 
-    thulac_res = open('res_tokenize/thulac_res_v1.txt', 'w')
+    pre_path = '/home/intern2017/huangyj/data/baike_data/part-r-000'
+    pre_path = 'local_test/part-r-000'
 
-    nlpir_res = open('res_tokenize/nlpir_res_v1.txt', 'w')
-
-    for i in range(1, 32):
-        pre_path = '/home/intern2017/huangyj/data/baike_data/part-r-000'
-
-        if i <= 9:
-            text_path = pre_path + '0' + str(i)
-        else:
-            text_path = pre_path + str(i)
-
-        with open("../htmltest.txt") as f:
-            rfgen = f.xreadlines()
-            for pyin in rfgen:
-                # 处理完标签后的数据，未分词，pyin是str类型，用decode utf8解码，可encode变回去str
-                buff = data_filter.textFormator(pyin.decode('utf-8'))
-                buff = buff.replace(' ', '').strip()
-                buff = data_filter.filter_pure_num(buff)
-                if len(buff) < 2:
-                    # print buff
-                    continue
-                ltp_res.write(at.ltp_tokenize(words_set, buff, True))
-                # nlpir_res.write(at.nlpir_tokenize(words_set, buff, True))
-                thulac_res.write(at.thulac_tokenize(words_set, buff, True))
-                jieba_res.write(at.jieba_tokenize2(words_set, buff, False))
-        print 'done'
-        break
+    with open(pre_path + num) as f:
+        rfgen = f.xreadlines()
+        for pyin in rfgen:
+            # 处理完标签后的数据，未分词，pyin是str类型，用decode utf8解码，可encode变回去str
+            buff = data_filter.textFormator(pyin.decode('utf-8'))
+            buff = buff.replace(' ', '').strip()
+            buff = data_filter.filter_pure_num(buff)
+            if len(buff) < 2:
+                # print buff
+                continue
+            ltp_res.write(at.ltp_tokenize(words_set, buff, True))
+            # nlpir_res.write(at.nlpir_tokenize(words_set, buff, True))
+            thulac_res.write(at.thulac_tokenize(words_set, buff, True))
+            jieba_res.write(at.jieba_tokenize2(words_set, buff, True))
+    print 'done', text_out_num
